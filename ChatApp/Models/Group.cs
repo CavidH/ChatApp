321fe0a19a4b3e2ad888dd05f29c51bae509154a
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-17 19:01:10 +0400

    :rocekt:

---
+++ ChatApp/Models/Group.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace ChatApp.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ModifiedTime { get; set; }
+        public ICollection<User> Users { get; set; }
+        public ICollection<Message> Messages { get; set; }
+
+        
+        
+    }
+}
---
+++ ChatApp/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models
+{
+    public class Message
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime MsgTime { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+    }
+}
