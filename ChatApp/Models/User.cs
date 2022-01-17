b64e8cc310e945b79c79d7a3b8d66e95a4aa3e48
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-17 18:40:12 +0400

    :rocket:

--- ChatApp/Models/User.cs
+++ ChatApp/Models/User.cs
@@ -2,9 +2,11 @@
 {
     public class User
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string UserName { get; set; }
         public int GroupId { get; set; }
         public Group Group { get; set; }
+      
+        
     }
 }
