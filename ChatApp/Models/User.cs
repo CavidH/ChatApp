dded856bad09976ff15cf63a4a0b7da7c60e95fd
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-17 18:17:07 +0400

    :rocket:

---
+++ ChatApp/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+    }
+}
