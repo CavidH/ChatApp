6a104a8d7d37ccedd51f4fa977ff125196372705
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-18 11:26:57 +0400

    bug fix

--- ChatApp/Hubs/ChatHub.cs
+++ ChatApp/Hubs/ChatHub.cs
@@ -3,20 +3,21 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace ChatApp.Hubs
 {
-    public class ChatHub:Hub
+    public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string group, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
-        public async Task AddGroupAsync(string group)
-        { 
-            await Groups.AddToGroupAsync(Context.ConnectionId, group);
-        }
-        public async Task RemoveGroupAsync(string group)
-        { 
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, group);
+            await Clients.Group(group).SendAsync("ReceiveMessage", user, message);
         }
 
+        public async Task AddGroupAsync(string group)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task RemoveGroupAsync(string group)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, group);
+        }
     }
 }
--- ChatApp/wwwroot/js/chat.js
+++ ChatApp/wwwroot/js/chat.js
@@ -1,6 +1,24 @@
 ﻿"use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+connection.on("ReceiveMessage", function (user, message) {
+    let li = ` <li class="list-group-item">
+                    <b>${user}</b>
+                    <p>${message}</p>
+                </li>`;
+    document.getElementById("messages").innerHTML += li;
+
+});
+connection.start().then(function () {
+    if (localStorage.getItem("user")){
+        showArea();
+        let user = JSON.parse(localStorage.getItem("user"));
+        connection.invoke("AddGroupAsync",user.group)
+    }
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
 
 let enterGroupForm = document.getElementById("enterGroupForm");
 let leaveGroupBtn = document.getElementById("leaveGroupBtn")
@@ -14,29 +32,31 @@ enterGroupForm.addEventListener("submit", function (ev) {
     }
     localStorage.setItem("user", JSON.stringify(user));
     showArea();
-    document.getElementById("joinArea").classList.add("d-none");
+    connection.invoke("AddGroupAsync",user.group)
 })
 
 leaveGroupBtn.addEventListener("click", function (ev) {
     localStorage.removeItem("user");
     document.getElementById("messageArea").classList.add("d-none");
     document.getElementById("joinArea").classList.remove("d-none");
+    connection.invoke("RemoveGroupAsync",user.group)
+
+
 })
 
 SendMessageForm.addEventListener("submit", function (ev) {
     ev.preventDefault();
     let message = document.querySelector("textarea").value;
     let user = JSON.parse(localStorage.getItem("user"));
-    let li = ` <li class="list-group-item">
-                    <b>${user.username}</b>
-                    <p>${message}</p>
-                </li>`;
-    document.getElementById("messages").innerHTML += li;
+    connection.invoke("SendMessage",user.username,user.group,message)
     document.querySelector("textarea").value = "";
 
 })
 
 function showArea() {
     document.getElementById("messageArea").classList.remove("d-none");
-    document.getElementById("joinArea").classList.remove("d-none");
+    document.getElementById("joinArea").classList.add("d-none");
+    document.getElementById("messages").innerHTML = "";
+
+
 }
